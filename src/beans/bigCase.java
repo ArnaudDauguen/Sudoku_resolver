15f6c7c7da1dc31800613489f7987669ed389da6
ArnaudD Windows <arnaud.dauguen@orange.fr>
2019-04-18 12:55:35 +0200

    début raisonnement

---
+++ src/beans/bigCase.java
@@ -0,0 +1,45 @@
+package beans;
+
+public class bigCase {
+
+	
+	private simpleCase[] cases = new simpleCase[9];
+	
+	private int[][] potentialsValuesForColumns = new int[1][3];
+	private int[][] potentialsValuesForLines = new int[1][3];
+	
+	
+	
+	
+	/*
+	une case :
+	
+	champs
+		[pttcase]
+		
+		[[int], [int], [int]] potialsValuesForColumns
+		[[int], [int], [int]] potialsValuesForLines
+		
+		
+	méthodes
+		getLine(x)               //getLine(simpleCase.getposX())
+		getColumn(y)
+		
+		blockColumn(int colone) => [int] (regarder chaques cases pour bloquer)
+		blockLine(int line) => [int] (regarder chaques cases pour bloquer)
+			
+		
+	
+	
+	
+	
+	*/
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/beans/simpleCase.java
@@ -0,0 +1,44 @@
+package beans;
+
+public class simpleCase {
+
+	
+	private int value;
+	private int posX;
+	private int posY;
+	
+	private int[] potentials = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	
+	public simpleCase(int value, int posX, int posY) {
+		this.value = value;
+		this.posX = posX;
+		this.posY = posY;
+		
+	}
+	
+	
+	
+	/*
+	une case :
+	
+	champs
+		int value
+		[int] liste de pententielles nombre
+		int posX
+		int posY
+		
+		
+	méthodes
+		setValue
+		getValue
+		getPosX
+		getPosY
+		regarder le gros carré
+			
+		
+	
+	
+	
+	
+	*/
+}
