ddef1b42bb097958816b351981afe425e7ad582a
Quentin Gans <quentin.gans@outlook.fr>
2019-04-18 11:26:46 +0200

    Fix bug

--- src/launchers/resolver.java
+++ src/launchers/resolver.java
@@ -1,9 +1,14 @@
 package launchers;
 
 public class resolver {
+	
+	private int[][] sudoBoard = {};
+	
+	public resolver() {
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
