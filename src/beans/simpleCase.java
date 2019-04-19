099ebf88daaa0dac493fd05e3e543f2a6c2a0f4e
Quentin Gans <quentin.gans@outlook.fr>
2019-04-19 11:47:23 +0200

    add private methods

--- src/beans/simpleCase.java
+++ src/beans/simpleCase.java
@@ -1,7 +1,6 @@
 package beans;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class simpleCase {
 	
--- src/launchers/resolver.java
+++ src/launchers/resolver.java
@@ -1,7 +1,10 @@
 package launchers;
 
-public class resolver {
-	
+import beans.board;
+
+public class resolver {	
+
+	private board sudoBoard;
 	
 	public resolver() {
 		
@@ -10,5 +13,37 @@ public class resolver {
 	public static void main(String[] args) {
 		
 	}
+	
+	 
+	
+	// Méthodes
+	
+	private boolean checkIfIsInTheLine(int c, int value) {
+ 		for(int l = 0; l < 9; l++) {
+ 			if(sudoBoard[l][c] == value) return true;
+ 		}
+ 		return false;
+ 	}
+	 	
+
+	private boolean checkIfIsInTheColumn(int l, int value) {
+ 		for(int c = 0; c < 9; c++) {
+ 			if(sudoBoard[l][c] == value) return true;
+ 		}
+ 		return false;
+ 	}
+	
+ 	
+ 	private boolean checkBigSquarre(int c, int l, int value) {
+ 		for(int col = 0; col < 3; col++) {
+ 			for(int row = 0; row < 3; row++) {
+ 				if(sudoBoard[l][c] == value) return true;
+ 			}
+ 		}
+ 		return false;
+ 	}
+	 
+	 
+	 
 
 }
