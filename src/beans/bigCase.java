8df2e2908a7d58f26ef37e3fc522ea21a89a6174
ArnaudD Windows <arnaud.dauguen@orange.fr>
2019-05-02 17:23:45 +0200

    Explications commentatives

--- src/beans/bigCase.java
+++ src/beans/bigCase.java
@@ -11,8 +11,9 @@ public class bigCase {
 	private int[][][] potentialsValues = new int[3][3][9];
 	
 	
-	//constructor
+	// Constructor
 	public bigCase(ArrayList<simpleCase> casesList) {
+		// Remplissage des valeurs potentielles
 		for(int l = 0; l < 3; l++) {
 			for(int c = 0; c < 3; c++) {
 				for(int nb = 1; nb <= 9; nb++) {
@@ -21,7 +22,7 @@ public class bigCase {
 			}
 		}
 		
-		// mise en place des petits carrés
+		// Mise en place des petits carrés
 		for(int x = 0; x < 3; x++) {
 			for(int y = 0; y < 3; y++) {
 				cases[x][y] = casesList.get(x + y);
@@ -32,7 +33,7 @@ public class bigCase {
 	
 	
 	
-	
+	// Recuperation d'une ligne de carre
 	public ArrayList<Integer> getLine(int x) {
 		ArrayList<Integer> listL = new ArrayList<Integer>();
 		for(int i = 0; i < 3; i++) {
@@ -41,6 +42,7 @@ public class bigCase {
 		return listL;
 	}
 	
+	// Recuperation d'une colonne de carre
 	public ArrayList<Integer> getColumn(int y) {
 		ArrayList<Integer> listC = new ArrayList<Integer>();
 		for(int i = 0; i < 3; i++) {
@@ -53,13 +55,14 @@ public class bigCase {
 	
 	
 	
-	
+	// Nettoie une ligne
 	public void cleanLine(int l, int value) {
 		for(int c = 0; c < 3; c++) {
 			potentialsValues[l][c][value -1] = 0; 
 		}
 	}
 	
+	// Nettoie une colonne
 	public void cleanColumn(int c, int value) {
 		for(int l = 0; l < 3; l++) {
 			potentialsValues[l][c][value -1] = 0; 
@@ -74,6 +77,8 @@ public class bigCase {
 	
 	
 	
+
+	// Getters && Setters
 	
 	public simpleCase[][] getsimpleCases(){
 		return cases;
